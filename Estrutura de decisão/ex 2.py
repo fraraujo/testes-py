6c6b509a3e46b1df63aff8f9360992f6ea5df015
Franklin Araujo <62389007+fraraujo@users.noreply.github.com>
2020-04-13 13:33:09 -0300

    Add files via upload

---
+++ Estrutura de decisão/ex 2.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+n1 = int(input("Digite um valor: "))
+
+if n1 >= 0:
+    print(n1, "é positivo.")
+else:
+    print(n1, "é negativo.")
