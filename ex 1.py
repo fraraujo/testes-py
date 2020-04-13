11bc747cd5b3acf90c9f7af362c37a18e7d53e7a
Franklin Araujo <62389007+fraraujo@users.noreply.github.com>
2020-04-13 13:33:21 -0300

    Delete ex 1.py

--- ex 1.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
-
-if n1 > n2:
-    print(n1, "é maior que", n2, ".")
-elif n2 > n1:
-    print(n2, "é maior que", n1, ".")
-else:
-    print("Os numeros são iguais.")
